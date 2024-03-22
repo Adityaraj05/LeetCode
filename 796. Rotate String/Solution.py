52380779146395f7f5143331315678e83d95e7b4
Aditya raj <118068294+Adityaraj05@users.noreply.github.com>
2024-03-22 17:44:14 +0530

    Update Solution.py

--- 796. Rotate String/Solution.py
+++ 796. Rotate String/Solution.py
@@ -5,5 +5,18 @@ class Solution:
             # here we are checking the abcde string is present in the goal+goal string is not -->cdeabcdeab 
             if s in goal+goal:   # abcde ---> cdeabcdeab  yes 
                 return True
-            
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:    
+        l = len(s)
+        s = list(s)
+        while l:
+            a = s.pop(0)
+            s.append(a)
+            if s == list(goal):
+                return True
+            l = l -1 
+        return False
         

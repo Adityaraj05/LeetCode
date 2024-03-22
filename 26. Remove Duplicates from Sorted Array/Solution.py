4e4c0ab44e7c733e75a09db6039676023926f357
Aditya raj <118068294+Adityaraj05@users.noreply.github.com>
2024-03-22 20:30:22 +0530

    Update Solution.py

--- 26. Remove Duplicates from Sorted Array/Solution.py
+++ 26. Remove Duplicates from Sorted Array/Solution.py
@@ -1,16 +1,17 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         n = len(nums)
-        if not n:
-            return 0
-        # This line initializes the variable duplicate to 1. This variable will be used to keep track of the length of the resulting list with duplicate elements removed. We start at 1 because we're assuming the first element of the list is unique.
-        duplicate = 1
-        for i in range(1, n):
-            # This line checks if the current element nums[i] is different from the previous unique element nums[count-1]. If it is different, it means we've encountered a new unique element.
-            if nums[i] != nums[duplicate-1]:
-                # If the current element is different from the previous unique element, we assign the current element to the position count in the list nums (which corresponds to the next position for a unique element). Then, we increment the count variable by 1 to indicate that we've found another unique element.
-                nums[duplicate] = nums[i]
-                duplicate+=1
-        return duplicate
+        # This line initializes a variable left to 1. The variable left will be used to keep track of the position where the next unique element should be placed in the modified list.
+        left = 1
+        # This line initiates a loop that iterates through the indices of the input list nums, starting from 1 (as the first element is considered already unique) up to the length of the list.
+        for right in range(1,len(nums)):
+            # This line checks if the current element (nums[right]) is different from the previous element (nums[right-1]). If they are different, it means the current element is unique.
+            if nums[right] != nums[right-1]:
+                # This line assigns the current unique element (nums[right]) to the position indicated by left in the list nums. Since left initially starts at 1, it means the first unique element will be placed at index 1 of the list.
+                nums[left] = nums[right]
+                # This line increments the left pointer by 1, indicating that the next unique element should be placed at the next position in the list.
+                left +=1
+        return left
 
+   
   

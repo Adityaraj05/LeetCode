cdcf41696530587dd1b48c22ec427e402cda7a91
Aditya raj <118068294+Adityaraj05@users.noreply.github.com>
2024-04-01 17:37:01 +0530

    Create Solution.py

---
+++ 171. Excel Sheet Column Number/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        ans = 0
+        dic = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+        for i in columnTitle: #"AB"
+            # ans = 26 * 0 + dic['A'] = 26 * 0 + 1 = 1 , ans = 26 * 1 + dic['B'] = 26 * 1 + 2 = 28
+            ans = 26 * ans + dic[i]
+        return ans

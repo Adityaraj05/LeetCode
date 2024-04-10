13d0802b2fd288ffa87dede1db563c0e85652920
Aditya raj <118068294+Adityaraj05@users.noreply.github.com>
2024-04-10 10:33:02 +0530

    Update Solution.py

--- 21. Merge Two Sorted Lists/Solution.py
+++ 21. Merge Two Sorted Lists/Solution.py
@@ -1,3 +1,5 @@
+# JAVA
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -26,4 +28,34 @@ class Solution {
         return list1;
     }
 }
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Pyhton
+
+
+# inition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+        return dummy.next
+        
         

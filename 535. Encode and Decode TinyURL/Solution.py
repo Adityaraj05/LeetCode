c0bc8d0e23b4cf5e6e0c8e2fb44e505a3d0fa57e
Aditya raj <118068294+Adityaraj05@users.noreply.github.com>
2024-05-09 17:40:36 +0530

    Create Solution.py

---
+++ 535. Encode and Decode TinyURL/Solution.py
@@ -0,0 +1,11 @@
+class Codec:
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        return longUrl
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        return shortUrl

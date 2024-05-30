32b0a7bf9ba6d35b4916e2a20f5f5bb0248fbda2
Aditya raj <118068294+Adityaraj05@users.noreply.github.com>
2024-05-30 08:10:27 +0530

    Create Solution.py

---
+++ 55. Jump Game/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+        
+        return True

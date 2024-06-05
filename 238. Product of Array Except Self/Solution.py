bc773f24773baf4aeaef3b8bdeaf7f857f82dfa3
Aditya raj <118068294+Adityaraj05@users.noreply.github.com>
2024-06-05 21:33:08 +0530

    Update Solution.py

--- 238. Product of Array Except Self/Solution.py
+++ 238. Product of Array Except Self/Solution.py
@@ -1,14 +1,22 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # initialize the result array with the same length as nums, filled with 1's. So initially, result = [1, 1, 1, 1].
         result = [1] * (len(nums))
-
-        prefix =1
+        prefix = 1
         for i in range(len(nums)):
             result[i] = prefix
             prefix *= nums[i]
-        postfix =1
+            # result [1,1,2,6]
+        postfix = 1
         for i in range(len(nums)-1,-1,-1):
-            result[i] *= postfix
-            postfix *=nums[i]
+            result[i]*=postfix
+            postfix *= nums[i]
         return result
+
+
+
         

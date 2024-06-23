fb56229f10827bae1397fdee9eb055ffdfc97d35
Aditya raj <118068294+Adityaraj05@users.noreply.github.com>
2024-06-23 12:11:56 +0530

    Create Solution.py

---
+++ 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/Solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+
+        n = len(nums)
+        # initialize two empty lists to be used as heaps. max_heap will store the negative values to simulate a max-heap, and min_heap will store the values as they are for a min-heap.
+        max_heap = []
+        min_heap = []
+        
+        left = 0
+        right = 0
+        max_length = 0
+        
+        while right < n:
+            #  push the current element (pointed to by right) into both heaps. In max_heap, the value is negated to simulate a max-heap since Python's heapq module only provides a min-heap. The second element of each tuple is the index of the element in the array.
+            heappush(max_heap, (-nums[right], right))
+            heappush(min_heap, (nums[right], right))
+            
+            # ested while loop checks if the difference between the maximum and minimum values in the current window exceeds the given limit. If so, the window needs to be adjusted by moving the left pointer.
+            while -max_heap[0][0] - min_heap[0][0] > limit:
+                #  moves the left pointer to one position after the smallest index of the current maximum or minimum value in the heaps. This effectively shrinks the window from the left side.
+                left = min(max_heap[0][1], min_heap[0][1]) + 1
+                # inner while loop removes elements from max_heap that are outside the current window. The condition checks if the index of the root element in max_heap is less than left.
+                while max_heap[0][1] < left:
+                    heappop(max_heap)
+                # this inner while loop removes elements from min_heap that are outside the current window. The condition checks if the index of the root element in min_heap is less than left.
+                while min_heap[0][1] < left:
+                    heappop(min_heap)
+            
+            max_length = max(max_length, right -left + 1)
+            right += 1
+        
+        return max_length
